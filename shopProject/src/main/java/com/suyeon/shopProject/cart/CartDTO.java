7a8408bd6c6fde44cf930334be8879a804d3ca38
yeonchoux <yeon_choux@naver.com>
2025-10-23 13:54:29 +0900

    cartDTO에 대한 기본적인 설정

--- shopProject/src/main/java/com/suyeon/shopProject/cart/CartDTO.java
+++ shopProject/src/main/java/com/suyeon/shopProject/cart/CartDTO.java
@@ -1,5 +1,42 @@
 package com.suyeon.shopProject.cart;
 
 public class CartDTO {
+	
+	int cartId;
+	int userId;
+	int productId;
+	int quantity;
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return "CartDTO [cartId=" + cartId + ", userId=" + userId + ", productId=" + productId + ", quantity="
+				+ quantity + "]";
+	}
+	
+	
 
 }
